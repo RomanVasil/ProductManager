5ee7a3b6c82c83fc192180223c535d322716e661
RomanVasil <jostik85@outlook.com>
2022-06-04 13:53:54 +0400

    add repo and manager v.1.1

--- src/test/java/ru/netology/domain/ProductManagerTest.java
+++ src/test/java/ru/netology/domain/ProductManagerTest.java
@@ -15,7 +15,7 @@ class ProductManagerTest {
     @Test
     public void shouldSearchBy() {
 
-        Product product3 = new Product(3, "Java1", 10);
+        Product product3 = new Product(3, "Javan", 10);
         Product product4 = new Product(4, "Java", 10);
 
         rep.add(product3);
@@ -24,6 +24,22 @@ class ProductManagerTest {
         String name = "Java";
 
         Product[] expected = new Product[]{product4};
+        Product[] actual = rep.searchBy(name);
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSearchBy0() {
+
+        Product product5 = new Product(5, "Javan", 10);
+        Product product6 = new Product(6, "Java", 10);
+
+        rep.add(product5);
+        rep.add(product6);
+
+        String name = "Lost";
+
+        Product[] expected = new Product[]{};
         Product[] actual = rep.searchBy(name);
         assertArrayEquals(expected, actual);
     }
